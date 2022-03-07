42539bc8a3fbc09075134df8fec8912cef180b25
vsvx1 <vsvx13@naver.com>
2022-03-08 01:18:54 +0900

    Application과 HelloController 생성

---
+++ src/main/java/com/chan/book/springboot/Application.java
@@ -0,0 +1,15 @@
+package com.chan.book.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication으로 인해 스프링부트의 자동설정, 스프링 Bean읽기와 생성을 모두 자동으로 설정.
+//이 클래스는 항상 프로젝트의 최상단에 위치해야만 한다.
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args){
+        //Application 클래스는 앞으로 만들 프로젝트의 메인 클래스가 된다.
+        //SpringApplication.run을 통해 내장 WAS를 실행한다.
+        SpringApplication.run(Application.class, args);
+    }
+}
